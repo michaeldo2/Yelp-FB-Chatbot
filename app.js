849ffeb58bf4a73174b68d62dafa84b4b82e3d52
Michael H Do <michaeldo@berkeley.edu>
2016-08-20 23:37:32 -0700

    message input to lower case

--- app.js
+++ app.js
@@ -254,7 +254,8 @@ function receivedMessage(event) {
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
     // the text we received.
-    switch (messageText) {
+    var lowercase = messageText.toLowerCase();
+    switch (lowercase) {
       case 'image':
         sendImageMessage(senderID);
         break;
